db60e2e50f714c90f00f3f6776d6cbdbaf403377
Fbrend23 <158816892+Fbrend23@users.noreply.github.com>
2025-11-02 17:43:42 +0100

    feat(routes) Add auth route

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -7,10 +7,14 @@
 |
 */
 
+const AuthController = () => import('#controllers/auth_controller')
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router
+  .group(() => {
+    router.post('/register', [AuthController, 'register'])
+    router.post('/login', [AuthController, 'login'])
+    router.get('/me', [AuthController, 'me']).use(middleware.auth())
+  })
+  .prefix('/auth')
