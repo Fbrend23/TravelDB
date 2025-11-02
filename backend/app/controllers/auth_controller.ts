6ccbc12b2a765a6a09fa5dd48f0304dc600166d6
Fbrend23 <158816892+Fbrend23@users.noreply.github.com>
2025-11-02 17:40:09 +0100

    feath(auth) Add controller

---
+++ backend/app/controllers/auth_controller.ts
@@ -0,0 +1,43 @@
+// import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import { loginValidator, registerValidator } from '#validators/auth'
+import { HttpContext } from '@adonisjs/core/http'
+
+export default class AuthController {
+  //post auth/register
+  public async register({ request, response, auth }: HttpContext) {
+    //data fetch
+    const payload = await request.validateUsing(registerValidator)
+
+    //account creation
+    const user = await User.create(payload)
+    //access token
+    const token = await auth.use('api').createToken(user)
+
+    return response.created({ message: `Merci pour votre inscription ${user.username}`, token })
+  }
+
+  //POST auth/login
+  public async login({ request, response, auth }: HttpContext) {
+    //validation of email and password
+    const { email, password } = await request.validateUsing(loginValidator)
+    //verification of email and password
+    const user = await User.verifyCredentials(email, password)
+    //access token
+    const token = await auth.use('api').createToken(user)
+
+    return response.ok({ token })
+  }
+
+  //GET auth/me
+  public async me({ auth, response }: HttpContext) {
+    // push user infos
+    const user = auth.user!
+    return response.ok({ id: user.id, email: user.email, username: user.username })
+  }
+  // POST auth/logout
+  public async logout({ auth, response }: HttpContext) {
+    await auth.use('api').invalidateToken()
+    return response.ok({ message: 'Déconnecté' })
+  }
+}
