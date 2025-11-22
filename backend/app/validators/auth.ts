dcc38ea5ab9113732614b2a563b0203cb5a99361
Fbrend23 <158816892+Fbrend23@users.noreply.github.com>
2025-11-22 13:48:17 +0100

    fix(validators): Moved the errors providers to the top and added unique email validator

--- backend/app/validators/auth.ts
+++ backend/app/validators/auth.ts
@@ -1,5 +1,18 @@
 import vine, { SimpleMessagesProvider } from '@vinejs/vine'
+vine.messagesProvider = new SimpleMessagesProvider({
+  'username.required': 'Le nom d’utilisateur est obligatoire.',
+  'username.minLength': 'Le nom d’utilisateur doit faire au minimum 3 caractères.',
+  'username.maxLength': 'Le nom d’utilisateur ne peut pas dépasser 32 caractères.',
+  'username.regex': 'Le nom d’utilisateur ne peut contenir que des lettres et chiffres.',
+  'username.unique': 'Ce nom d’utilisateur est déjà utilisé.',
 
+  'email.required': 'L’adresse email est obligatoire.',
+  'email.email': 'Veuillez saisir une adresse email valide.',
+
+  'password.required': 'Le mot de passe est obligatoire.',
+  'password.minLength': 'Le mot de passe doit faire au minimum 8 caractères.',
+  'password.maxLength': 'Le mot de passe ne peut pas dépasser 512 caractères.',
+})
 export const loginValidator = vine.compile(
   vine.object({
     password: vine.string().minLength(8).maxLength(512),
@@ -18,21 +31,12 @@ export const registerValidator = vine.compile(
         return !user
       }),
     password: vine.string().minLength(8).maxLength(512),
-    email: vine.string().email(),
+    email: vine
+      .string()
+      .email()
+      .unique(async (query, field) => {
+        const user = await query.from('users').where('email', field).first()
+        return !user
+      }),
   })
 )
-
-vine.messagesProvider = new SimpleMessagesProvider({
-  'username.required': 'Le nom d’utilisateur est obligatoire.',
-  'username.minLength': 'Le nom d’utilisateur doit faire au minimum 3 caractères.',
-  'username.maxLength': 'Le nom d’utilisateur ne peut pas dépasser 32 caractères.',
-  'username.regex': 'Le nom d’utilisateur ne peut contenir que des lettres et chiffres.',
-  'username.unique': 'Ce nom d’utilisateur est déjà utilisé.',
-
-  'email.required': 'L’adresse email est obligatoire.',
-  'email.email': 'Veuillez saisir une adresse email valide.',
-
-  'password.required': 'Le mot de passe est obligatoire.',
-  'password.minLength': 'Le mot de passe doit faire au minimum 8 caractères.',
-  'password.maxLength': 'Le mot de passe ne peut pas dépasser 512 caractères.',
-})
