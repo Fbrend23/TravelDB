4b9b25168f6376a2910357c98c44133b8877744e
Fbrend23 <158816892+Fbrend23@users.noreply.github.com>
2025-11-30 22:10:36 +0100

    fix: moved boostrap to main.ts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,35 +4,7 @@ import Nav from '@/components/NavBar.vue'
 import Footer from '@/components/FooterComponent.vue'
 import { BApp } from 'bootstrap-vue-next'
 import 'leaflet/dist/leaflet.css'
-import "bootstrap-icons/font/bootstrap-icons.css";
 import { useThemeStore } from './stores/theme'
-import "@/assets/scss/custom.scss";
-import { createBootstrap } from 'bootstrap-vue-next'
-
-const bootstrap = createBootstrap({
-  global: {
-    ripple: true,
-  },
-  components: {
-    BButton: {
-      rounded: 'pill',
-      boxShadow: false,
-    },
-    BCard: {
-      borderVariant: 'none',
-      class: 'shadow-sm border-0',
-    },
-    BFormInput: {
-      size: 'lg',
-    },
-    BFormSelect: {
-      size: 'lg',
-    },
-    BNavbar: {
-      shadow: true,
-    }
-  }
-})
 const themeStore = useThemeStore()
 themeStore.initTheme()
 </script>
